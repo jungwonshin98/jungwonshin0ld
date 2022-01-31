08ca042d85aeb980b487675b8d21651608966699
Jungwon Shin <Jungwon.shin98@gmail.com>
2022-01-31 16:41:55 -0500

    a new project added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,6 +5,23 @@
 //     $('.preloader').fadeOut(1000); // set duration in brackets    
 // });
 
+/* Collapsible
+-----------------------------------------------*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    // this.text("Hide Product Description");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
  /* Google Map
 -----------------------------------------------*/
